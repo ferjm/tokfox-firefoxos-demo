5e2d09fa96e4c26f557f81f28f39ad55e5153567
borjasalguero <fbsc@tid.es>
2014-03-12 18:34:10 +0100

    UI Manager added

--- js/startup.js
+++ js/startup.js
@@ -5,32 +5,9 @@
 
 /* global Notifications, TokFoxClient */
 
-var StartUp = {
-  SIGN_UP_KEY: 'user.logged',
-
-  init: function tfd_init() {
-    var alias = {};
-    alias.type = 'msisdn';
-    alias.value = '+34666777888';
-
-    this.signupsignin(alias, function onSignUpSignIn() {
-    });
-  },
-
-  /**
-   * Sign Up / Sing in the user. Performs the PUSH registration and account
-   * creation dance.
-   *
-   * @param {Object} alias JSON object containing user's MSISDN.
-   * @param {Function} callback Callback function to call once the sign in/up
-   *                            process is completed.
-   */
-  signupsignin: function tfd_signupsignin(alias, callback) {
-  }
-};
-
 window.addEventListener('load', function callSetup(evt) {
   window.removeEventListener('load', callSetup);
 
-  StartUp.init();
-});
+  UIManager.init();
+  UIManager.register();
+ });
---
+++ js/ui_manager.js
@@ -0,0 +1,65 @@
+'use strict';
+
+(function(exports) {
+  var _initialized = false;
+  var hangupButton, countryCodeSelect, phoneNumberInput, sendMSISDN;
+  var contactNameDOM, contactPhoneNumberDOM;
+  var UIManager = {
+    init: function() {
+      if (_initialized) {
+        return;
+      }
+      hangupButton = document.getElementById('call-hangup');
+      countryCodeSelect = document.getElementById('country-code');
+      phoneNumberInput = document.getElementById('telephone');
+      sendMSISDN = document.getElementById('send-msisdn-button');
+      contactNameDOM = document.getElementById('call-contact-name');
+      contactPhoneNumberDOM = document.getElementById('call-contact-phone');
+
+      phoneNumberInput.addEventListener('input', function() {
+        if (phoneNumberInput.value.length > 0) {
+          sendMSISDN.classList.remove('disabled');
+        } else {
+          sendMSISDN.classList.add('disabled');
+        }
+      });
+
+      
+      _initialized = true;
+    },
+    call: function(name, phoneNumber, onHangup) {
+      document.body.dataset.layout = 'call';
+
+      if (!name) {
+        contactNameDOM.textContent = phoneNumber;
+        contactPhoneNumberDOM.textContent = 'Tokfox user';
+      } else {
+        contactNameDOM.textContent = name;
+        contactPhoneNumberDOM.textContent = phoneNumber;
+      }
+
+      hangupButton.addEventListener('click', function hang() {
+        hangupButton.removeEventListener('click', hang);
+        if (typeof onHangup === 'function') {
+          onHangup();
+        }
+      });
+    },
+    register: function(onRegistered) {
+      document.body.dataset.layout = 'register';
+      sendMSISDN.addEventListener('click', function send() {
+        sendMSISDN.removeEventListener('click', send);
+        if (typeof onRegistered === 'function') {
+          var phone =
+            countryCodeSelect.options[countryCodeSelect.selectedIndex].value +
+            phoneNumberInput.value;
+          onRegistered(phone);
+        }
+      });
+    }
+  };
+
+  exports.UIManager = UIManager;
+}(this));
+
+UIManager.init();
