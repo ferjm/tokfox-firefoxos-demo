b57ff345e91ea3b8868ea8da4991ddfdc22e3d96
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2014-03-13 11:39:33 +0100

    Sign up, sign in.

--- js/activity_handler.js
+++ js/activity_handler.js
@@ -1,4 +1,11 @@
+/* -*- Mode: js; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
 'use strict';
+
+/* global UIManager, CallHandler, Notifications */
+
+/* exported ActivityHandler */
 
 (function(exports) {
   function _handle(activity) {
@@ -11,6 +18,38 @@
       return;
     }
 
+    if (!localStorage['msisdn']) {
+      UIManager.register(function(phoneNumber) {
+        Notifications.register(function onRegister(r_error, r_result) {
+          if (!r_error) {
+            TokFoxClient.createAccount(
+              'msisdn',
+              phoneNumber,
+              r_result.endPoint,
+              function(ca_error, ca_result) {
+                if (!ca_error) {
+                  _makeCall(number);
+                  localStorage['msisdn'] = number;
+                  return;
+                }
+                // TODO: Handle error if any.
+                alert('Unable to register the user.');
+                window.close();
+                return;
+            });
+            return;
+          }
+          // TODO: Improve the way this error is handled.
+          alert('Unable to register the user.');
+          window.close();
+        });
+      });
+    } else {
+      _makeCall(number);
+    }
+  }
+
+  function _makeCall(number) {
     UIManager.call(null, number, function() {
       window.close();
     });
@@ -19,7 +58,7 @@
        type: 'msisdn',
        alias: number
     }, function() {
-      console.log('Yay!');
+      // TODO NiceToHave: Update the UI consequently
     });
   }
 
